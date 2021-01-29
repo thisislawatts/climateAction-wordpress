93d2cdd651bbe127af9cba81f7789bb2dbd1c631
hanopcan <hannah@opcan.co.uk>
2021-01-29 12:14:58 +0000

    Only display blog widget on single posts, not pages as well

--- inc/blog.php
+++ inc/blog.php
@@ -9,11 +9,11 @@ add_action( 'neve_after_content', 'cat_output_blog_footer' );
  * Output widget area at the bottom of a single blog post.
  */
 function cat_output_blog_footer() {
-	if ( is_active_sidebar( 'cat-blog-footer' )  ) :
+	if ( is_active_sidebar( 'cat-blog-footer' ) && is_single() ) :
 
 		echo '<div class="widget-area" role="complementary">';
 		dynamic_sidebar( 'cat-blog-footer' );
 		echo '</div>';
-	
+
 	endif;
 }
