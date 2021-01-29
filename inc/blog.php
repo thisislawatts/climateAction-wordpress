15174adee07ba716f4f8208333ccfebde2a1510a
hanopcan <hannah@opcan.co.uk>
2021-01-29 15:43:30 +0000

    Move blog footer to right at the end of a post

--- inc/blog.php
+++ inc/blog.php
@@ -3,13 +3,13 @@
  * Customisations to the blog posts
  */
 
-add_action( 'neve_after_content', 'cat_output_blog_footer' );
+add_action( 'neve_after_post_content', 'cat_output_blog_footer' );
 
 /**
  * Output widget area at the bottom of a single blog post.
  */
 function cat_output_blog_footer() {
-	if ( is_active_sidebar( 'cat-blog-footer' ) && is_single() ) :
+	if ( is_active_sidebar( 'cat-blog-footer' ) && is_singular( 'post' ) ) :
 
 		echo '<div class="widget-area" role="complementary">';
 		dynamic_sidebar( 'cat-blog-footer' );
